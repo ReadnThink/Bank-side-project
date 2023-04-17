70d5131f1368d5ebf4f3b1d4865bbe8714a75a35
solbae <sol4854@naver.com>
2023-04-17 18:06:00 +0900

    first commit

---
+++ src/main/java/com/side/workout/domain/user/UserEnum.java
@@ -0,0 +1,11 @@
+package com.side.workout.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserEnum {
+    ADMIN("관리자"), CUSTOMER("고객");
+    private String value;
+}
